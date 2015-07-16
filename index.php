a76e12aa25ee750f0db12bfd250b921bed17380e
YouMereIllusion <melina.darocha@gmail.com>
2015-07-16 19:44:18 +0200

    Struct HTML
    Header + section + footer

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
 </head>
 <body>
 
+<header>
+    <div>
+        <p>Une image ou un logo</p>
+    </div>
+    <div>
+        <h1>Mélina Da Rocha</h1>
+        <h2>Multimedia student</h2>
+    </div>
+</header>
+
+<section></section>
+
+<footer></footer>
 
 </body>
 </html>
